8a02883094e4ee5fa5cb0817873671ad56a41be2
Sujauddin <61101752+sujaudd1n@users.noreply.github.com>
2024-02-03 00:14:26 +0530

    Fix baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://sujaudd1n.github.io/',
+  url: 'https://opencalisthenics.netlify.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/calisthenics/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
